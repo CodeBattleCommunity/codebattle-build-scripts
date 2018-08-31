448a43301805c8e88c1b4ad8c5aa6280a1df003b
Pavel M <pavel_mezrin@epam.com>
2018-08-31 14:08:47 +0400

    Add script to start mongo in docker

---
+++ mongo/install/start-mongo.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+docker run --rm -d -p 27017:27017 --name codebattle-mongo -v codebattle-mongo-db:/data/db \
+    -v /root/data/mongo/secrets:/run/secrets \
+    -e MONGO_INITDB_ROOT_USERNAME_FILE=/run/secrets/mongo-root-username  \
+    -e MONGO_INITDB_ROOT_PASSWORD_FILE=/run/secrets/mongo-root-password mongo
