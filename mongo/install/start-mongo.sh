7a1ea3db25e99657b6b7701ffa7fd2e635c4e441
Pavel M <pavel_mezrin@epam.com>
2018-09-06 17:26:39 +0400

    Use turn_for_battlecity branch due to absence of privileges to merge to master

--- mongo/install/start-mongo.sh
+++ mongo/install/start-mongo.sh
@@ -3,4 +3,5 @@
 docker run --rm -d -p 27017:27017 --name codebattle-mongo -v codebattle-mongo-db:/data/db \
     -v /root/data/mongo/secrets:/run/secrets \
     -e MONGO_INITDB_ROOT_USERNAME_FILE=/run/secrets/mongo-root-username  \
-    -e MONGO_INITDB_ROOT_PASSWORD_FILE=/run/secrets/mongo-root-password mongo
+    -e MONGO_INITDB_ROOT_PASSWORD_FILE=/run/secrets/mongo-root-password \
+    mongo
